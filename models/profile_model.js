7a362b83c8539776ccbd5b3d3b7613380d676b36
ebisuamagi <thedeadneverdies@gmail.com>
2022-02-05 11:10:01 -0800

    Profile Model

---
+++ models/profile_model.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool();
+
+/** 
+ * createProfile
+ *      Inserts a profile object into the database
+ *      Returns the created profile id
+ * @param {*} data 
+ */
+export async function createProfile(profileInfo){
+    const query = {
+        text: `INSERT INTO profile 
+               (userid)
+               VALUES (($1))
+               RETURNING profileid`,
+        values: [profileInfo.userid]
+    }
+
+    console.log(await pool.query(query));
+}
