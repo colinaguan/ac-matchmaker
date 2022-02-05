1c1badb448708908d179ad51ab8e7f3d7126de16
ebisuamagi <thedeadneverdies@gmail.com>
2022-02-05 11:46:23 -0800

    Installed a ESLINT Google Style guide enforced, also made some fixes to style on all files

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const path = require('path');
-const user_model = require("./models/user_model");
+const userModel = require('./models/user_model');
+// const profileModel = require('./models/profile_model');
 const uuid = require('uuid');
 require('dotenv').config();
 
@@ -11,78 +12,78 @@ app.use(express.json());
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
   );
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers"
+      'Access-Control-Allow-Headers',
+      'Content-Type, Access-Control-Allow-Headers',
   );
   next();
 });
 
 
-app.get("/api/users", (req, res) => {
-    user_model
-        .getUsers()
-        .then((response) => {
-            res.status(200).send(response);
-        })
-        .catch((error) => {
-            res.status(500).send(error);
-        });
-});
-
-app.post("/api/login", (req, res) => {
-  user_model
-      .getUser(req.body.useremail)
+app.get('/api/users', (req, res) => {
+  userModel
+      .getUsers()
       .then((response) => {
-          const crypt = (bcrypt.compare(req.password, response.userpassword, function (err, isValid) {
-            if (err) return "error";
-
-            return true
-          }))
-          if (crypt != "error") { 
-            delete response[0].userpassword;
-            res.status(200).send(response);
-          }
+        res.status(200).send(response);
       })
       .catch((error) => {
-          res.status(500).send(error);
+        res.status(500).send(error);
+      });
+});
+
+app.post('/api/login', (req, res) => {
+  userModel
+      .getUser(req.body.useremail)
+      .then((response) => {
+        const crypt = (bcrypt.compare(req.password,
+            response.userpassword, function(err, isValid) {
+              if (err) return 'error';
+              return true;
+            }));
+        if (crypt != 'error') {
+          delete response[0].userpassword;
+          res.status(200).send(response);
+        }
+      })
+      .catch((error) => {
+        res.status(500).send(error);
       });
 });
 
 
-app.post("/api/userCreation", (req, res) => {
-    const myPlaintextPassword = req.body.userpassword;
-    const hash = bcrypt.hashSync(myPlaintextPassword, salt);
-    req.body.userpassword = hash;
-    newUUID = uuid.v4();
+app.post('/api/userCreation', (req, res) => {
+  const myPlaintextPassword = req.body.userpassword;
+  const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+  req.body.userpassword = hash;
+  newUUID = uuid.v4();
 
-    user_model.createUser(req.body, newUUID)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-  })
+  userModel.createUser(req.body, newUUID)
+      .then((response) => {
+        res.status(200).send(response);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+});
 
 // example api endpoint
 // app.get('/api/getList', (req,res) => {
-//     var list = ["item1", "item2", "item3"];
+//     var list = ['item1', 'item2', 'item3'];
 //     res.json(list);
 //     console.log('Sent list of items');
 // });
 
 // redirects any other paths to the client
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('*', function(req,res) {
-    console.log('trying to redirect to client build')
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('*', function(req, res) {
+  console.log('trying to redirect to client build');
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
--- models/profile_model.js
+++ models/profile_model.js
@@ -3,20 +3,20 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool();
 
-/** 
+/**
  * createProfile
  *      Inserts a profile object into the database
  *      Returns the created profile id
- * @param {*} data 
+ * @param {*} profileInfo
  */
-export async function createProfile(profileInfo){
-    const query = {
-        text: `INSERT INTO profile 
-               (userid)
-               VALUES (($1))
-               RETURNING profileid`,
-        values: [profileInfo.userid]
-    }
+export async function createProfile(profileInfo) {
+  const query = {
+    text: `INSERT INTO profile 
+            (userid)
+            VALUES (($1))
+            RETURNING profileid`,
+    values: [profileInfo.userid],
+  };
 
-    console.log(await pool.query(query));
+  console.log(await pool.query(query));
 }
