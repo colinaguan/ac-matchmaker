7d2a0c4bd8ecd180ab71af7aa7396c2bc42463a2
danielsarap96 <danielsarap96@gmail.com>
2022-04-19 12:42:18 -0700

    Made email verification link dynamic

--- client/src/app/util/EmailVerification.js
+++ client/src/app/util/EmailVerification.js
@@ -1,16 +1,24 @@
-import emailjs, {init} from '@emailjs/browser';
-init('5A9OSqglcYcpKAMgd');
+// import emailjs, {init} from '@emailjs/browser';
+// init('5A9OSqglcYcpKAMgd');
 
 export const VerifyEmail = (user) => {
+  let route;
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+    route = `http://tassel.com/verify/${user.token}`;
+  } else {
+    route = `http://localhost:3000/verify/${user.token}`;
+  }
+
   const templateParams = {
     to_email: user.useremail,
     // to-do: Make message link dynamic so that we
     // dont have to switch for production or developement
-    message: `http://localhost:3000/verify/${user.token}`,
+    message: route,
   };
 
   if (templateParams.to_email != null) {
-    console.log('templateParams:', templateParams);
+    console.log(templateParams.message);
+    /* console.log('templateParams:', templateParams);
     emailjs.send('service_2ncxfor', 'template_np5ucmd', templateParams)
         .then(function(response) {
           console.log('SUCCESS!', response.status, response.text);
@@ -18,6 +26,7 @@ export const VerifyEmail = (user) => {
         }, function(error) {
           console.log('FAILED...', error);
           return ('Failed');
-        });
+        });*/
   }
 };
+
